1875a659bdaec2c1d9f999dd81db7f034ec6afd8
ThomasMGilman <Thomas.Gilman@ymail.com>
2019-04-02 13:12:07 -0400

    finish async lab

--- Assignment 14/Lightning Lab/Program.cs
+++ Assignment 14/Lightning Lab/Program.cs
@@ -1,14 +1,31 @@
-﻿using System;
+﻿//Thomas Gilman
+//James Hudson
+//Operating Systems 2
+//Lightning Lab Assignment 14
+//28th March, 2019
+using System;
 using System.IO;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 class Program
 {
     static int numDownloads;
+    static Dictionary<string, download> downloads;
     static int index;
-    static object Lock;
+
+    public class download
+    {
+        public string fileName;
+        public HttpResponseMessage response;
+        public float progress;
+        public download()
+        {
+            progress = 0;
+        }
+    }
 
     public static async Task<HttpResponseMessage> getHttpResponse(string address)
     {
@@ -17,10 +34,41 @@ class Program
         return await client.GetAsync(link);           //get response from address;
     }
 
-    public static async void downloadLink(string address)
+    public static async Task writeToFile(string fileName, string address, byte[] data)
+    {
+        try
+        {
+            FileStream file = new FileStream(fileName, FileMode.Create);
+            int amountWritten = 0;
+            int amountToWrite = data.Length / 100;
+            if (amountToWrite == 0)
+                amountToWrite = 200;
+
+            while (amountWritten < data.Length)
+            {
+                int toWrite;
+                if (amountToWrite <= (data.Length - amountWritten))
+                {
+                    toWrite = amountToWrite;
+                }
+                else
+                    toWrite = data.Length - amountWritten;
+                await file.WriteAsync(data, amountWritten, toWrite);
+                amountWritten += toWrite;
+                downloads[address].progress = (amountWritten / data.Length) * 100f;
+                //Console.WriteLine("{0} Size: {1} Wrote: {2}\nNow: {3} progress: {4}", address, data.Length, toWrite, amountWritten, downloads[address].progress);
+            }
+
+            file.Close();
+        }
+        catch(Exception e)
+        {
+        }
+    }
+
+    public static async Task downloadLink(string address)
     {
         string fileName;
-        FileStream file;
 
         //create FileName based on relative path of link
         int indexOfRelativ = address.LastIndexOf('/');
@@ -33,31 +81,17 @@ class Program
         else
             fileName = index++.ToString()+".html"; //no path found
 
-        var msg = getHttpResponse(address);
-        HttpResponseMessage response = msg.Result;
+        HttpResponseMessage response = await getHttpResponse(address);
+        downloads[address].response = response;
 
         if(response.IsSuccessStatusCode)                          //Http Connection Success
         {
-            if (File.Exists(fileName))   //if FileName already exists append number to it
-            {
-                int fileNum = 0;
-                while (File.Exists(fileName + fileNum))
-                    fileNum++;
-
-                fileName += fileNum;
-            }
             foreach (char c in Path.GetInvalidFileNameChars())  //remove invalid filename chars
                 fileName = fileName.Replace(c, ' ');
+            downloads[address].fileName = fileName;
 
-            var dataTsk = response.Content.ReadAsByteArrayAsync();  //Read contents of webdata to byte array to be written to a file
-            file = new FileStream(fileName, FileMode.Create);
-            await file.WriteAsync(dataTsk.Result, 0, dataTsk.Result.Length);
-            file.Close();
-            //Console.WriteLine("wrote {0} to {1}", address, fileName);
-        }
-        else
-        {
-            //Console.WriteLine("Failed to get ' {0} ' message", address);
+            byte[] data = await response.Content.ReadAsByteArrayAsync();  //Read contents of webdata to byte array to be written to a file
+            await writeToFile(fileName, address, data);
         }
         numDownloads--;
     }
@@ -66,7 +100,7 @@ class Program
     {
         if (args.Length != 0)
         {
-            Lock = new object();
+            downloads = new Dictionary<string, download>();
             string[] data = File.ReadAllLines(args[0]); //read in File content
             numDownloads = data.Length;
             index = 0;
@@ -76,7 +110,12 @@ class Program
                 string address = data[i].Trim();
                 try
                 {
-                    downloadLink(address);
+                    if (!downloads.ContainsKey(address)) //dont download the same link twice
+                    {
+                        download newDownload = new download();
+                        downloads.Add(address, newDownload);
+                        downloadLink(address);
+                    }
                 }
                 catch(Exception e)
                 {
@@ -84,12 +123,38 @@ class Program
                 }
             }
 
-            while (numDownloads > 0)
+            bool notFin = true;
+            while (notFin)
             {
+                notFin = false;
                 Thread.Sleep(1000);
+                foreach(KeyValuePair<string, download> pair in downloads)
+                {
+                    download d = pair.Value;
+                    string progress;
+                    if(d.response != null)
+                    {
+                        if(d.response.IsSuccessStatusCode)
+                        {
+                            if (d.progress != 100)
+                                notFin = true;
+                            progress = d.progress.ToString() + "%";
+                        }
+                        else
+                            progress = "DeadLink!!";
+                    }
+                    else
+                    {
+                        notFin = true;
+                        progress = "Waiting on response!!";
+                    }
+                    Console.WriteLine("{0} : {1}", pair.Key, progress);
+                }
+                Console.WriteLine("------------------------------------");
             }
 
             Console.WriteLine("All Done!!");
+            Console.Read();
         }
         else
             throw new Exception("Input Error!!! Need to pass the name of a file of links to download in \n'link newline link newline etc...' format");
