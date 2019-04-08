ff44c1dbd66e9c3037b1b10d4747886cc7271f11
ThomasGilman <Thomas.Gilman@ymail.com>
2019-04-08 16:53:13 -0400

    progress made on client server program

---
+++ Assignment 15/Chat Server/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Drawing;
+
+class MainClass : Form
+{
+    const int Port = 8000;
+    const string serverIP = "127.0.0.1";
+    bool done = false;
+    ClientInfo client;
+
+    public static void Main()
+    {
+        Application.Run(new MainClass());
+    }
+
+    class ClientInfo
+    {
+        public TcpClient c;
+        public StreamReader R;
+        public StreamWriter W;
+        public ClientInfo(TcpClient c)
+        {
+            this.c = c;
+            var strm = c.GetStream();
+            this.R = new StreamReader(strm);
+            this.W = new StreamWriter(strm);
+        }
+    }
+
+    static async Task sendMsg(string msg, ClientInfo client)
+    {
+        await client.W.WriteLineAsync(msg);
+        await client.W.FlushAsync();
+    }
+
+    static async Task<object> getMessage(ClientInfo c)
+    {
+        return await c.R.ReadLineAsync();
+    }
+
+    private void Form_Closing(object sender, FormClosingEventArgs e)
+    {
+        closeConnection();
+    }
+
+    private void closeConnection()
+    {
+        done = true;
+        sendMsg("EXIT", client);
+        client.c.GetStream().Close();
+        client.c.Close();
+        Environment.Exit(0);
+    }
+
+    public MainClass()
+    {
+        TcpClient c = new TcpClient(serverIP, Port);
+        client = new ClientInfo(c);
+        
+
+        this.Size = new Size(350, 400);
+        this.CenterToScreen();
+
+        var textEntry = new TextBox();
+        textEntry.Multiline = true;
+        textEntry.ScrollBars = ScrollBars.Both;
+        textEntry.Parent = this;
+        textEntry.Dock = DockStyle.Fill;
+        textEntry.ReadOnly = true;
+        textEntry.Text = "Welcome to Datcord!\n";
+
+        var pan2 = new TableLayoutPanel();
+        pan2.RowCount = 1;
+        pan2.ColumnCount = 2;
+        pan2.AutoSize = true;
+        pan2.Parent = this;
+        pan2.Dock = DockStyle.Top;
+
+        var lineEntry = new TextBox();
+        lineEntry.Size = new Size(250, lineEntry.Size.Height);
+        lineEntry.Dock = DockStyle.Left;
+        lineEntry.Parent = pan2;
+
+        var button = new Button();
+        button.Text = "Send";
+        button.Parent = pan2;
+        button.Dock = DockStyle.Right;
+        button.Click += (s, e) => {
+            sendMsg(lineEntry.Text, client);
+            lineEntry.Text = "";
+            this.ActiveControl = lineEntry;
+        };
+
+        MainMenu mbar = new MainMenu();
+        MenuItem file = new MenuItem("File");
+        MenuItem quit = new MenuItem("Quit", (s, e) => {
+            closeConnection();
+        });
+        file.MenuItems.Add(quit);
+        mbar.MenuItems.Add(file);
+        this.Menu = mbar;
+
+        this.ActiveControl = lineEntry;
+        this.AcceptButton = button;
+
+        var msgTsk = new Task(() =>
+        {
+            while(!done)
+            {
+                var msg = getMessage(client);
+                string message = msg.Result.ToString();
+                try
+                {
+                    this.Invoke((MethodInvoker)delegate ()
+                    {
+                        textEntry.AppendText(message + Environment.NewLine);
+                    });
+                }
+                catch(Exception)
+                {
+                    return;
+                }
+            }
+        });
+        msgTsk.Start();
+    }
+}
---
+++ Assignment15_1/Server/server.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Collections.Generic;
+
+class ClientInfo
+{
+    public TcpClient c;
+    public StreamReader R;
+    public StreamWriter W;
+    public ClientInfo(TcpClient c)
+    {
+        this.c = c;
+        var strm = c.GetStream();
+        this.R = new StreamReader(strm);
+        this.W = new StreamWriter(strm);
+    }
+}
+
+class MainClass
+{
+    const int Port = 8000;
+    const int clientCount = 8;
+    static int numAccepts;
+    static IPAddress localAdd = IPAddress.Parse("127.0.0.1");
+
+    static async Task<object> accept(TcpListener srv)
+    {
+        var X = await srv.AcceptTcpClientAsync();
+        Console.WriteLine("ACCEPTED!");
+        return X;
+    }
+
+    static async Task<object> getMessage(ClientInfo c)
+    {
+        var X = await c.R.ReadLineAsync();
+        Console.WriteLine("GOTTEN!");
+        return X;
+
+    }
+    static async Task<bool> sendMessage(ClientInfo c, string s)
+    {
+        Console.WriteLine("SEND? " + s);
+        try
+        {
+            await c.W.WriteLineAsync(s);
+            await c.W.FlushAsync();
+            Console.WriteLine("SENT!");
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    static void disconnectClient(ClientInfo c)
+    {
+        c.c.Close();
+    }
+
+    static void attemptSend(ref List<ClientInfo> clients, string s)
+    {
+        List<ClientInfo> unResponsive = new List<ClientInfo>();
+        foreach (var client in clients)
+        {
+            if (!sendMessage(client, s).Result)
+                unResponsive.Add(client);
+        }
+        if (unResponsive.Count > 0)
+        {
+            foreach (var client in unResponsive)
+            {
+                disconnectClient(client);
+                clients.Remove(client);
+                numAccepts++;
+            }
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        var srv = new TcpListener(localAdd, Port);
+        numAccepts = clientCount;
+        srv.Start();
+        var clients = new List<ClientInfo>();
+        var T = new List<Task<object>>();
+        T.Add(accept(srv));
+        while (true)
+        {
+            int idx = Task.WaitAny(T.ToArray());
+            if (T[idx].IsFaulted)
+            {
+                if (clients.Count > 1)
+                    attemptSend(ref clients, "User Left Chat!!");
+                else
+                {
+                    disconnectClient(clients[0]);
+                    clients.Remove(clients[0]);
+                    numAccepts++;
+                }
+            }
+            else
+            {
+                if (idx == 0)
+                {
+                    object ob = T[0].Result;
+                    TcpClient cl = ob as TcpClient;
+                    var cinfo = new ClientInfo(cl);
+                    clients.Add(cinfo);
+                    T[0] = accept(srv);
+                    T.Add(getMessage(cinfo));
+                }
+                else
+                {
+                    object ob = T[idx].Result;
+                    string s = ob as string;
+                    attemptSend(ref clients, s);
+                    T[idx] = getMessage(clients[idx - 1]);
+                    //Console.WriteLine(T[idx].Result);
+                }
+            }
+            if (numAccepts > 0)
+            {
+                T.Add(accept(srv));
+                numAccepts--;
+            }
+        }
+    }
+}
