b2ff9a387ec09ce73cacb443fc95929a5f2fb526
ThomasMGilman <Thomas.Gilman@ymail.com>
2019-04-18 17:48:51 -0400

    work done on lab 17

--- Assignment 17/MPI/Mandelbrot.cpp
+++ Assignment 17/MPI/Mandelbrot.cpp
@@ -12,15 +12,19 @@ const double xmin = -1.5;
 const double xmax = 0.5;
 const double ymin = -1;
 const double ymax = 1;
+int maxHeight;
+std::vector<uint8_t> globalData;
 
 struct workLoad {
 	int w, h;
 	int stride, miter;
-	int x, y;
-	std::vector<uint8_t> data;
+	int y;
 };
 
-void compute(std::vector<uint8_t>& data, int w, int h, int stride, int maxiter);
+void splitWork(std::vector<uint8_t>& data, int w, int h, int stride, int maxiter, int numTasks);
+void compute(int rank);
+std::vector<uint8_t> getMessage(int rank);
+//void compute(std::vector<uint8_t>& data, int w, int h, int stride, int maxiter);
 int iterationsToInfinity(double px, double py, int maxiter);
 void mapColor(int k, int MAX_ITERS, uint8_t rgb[3]);
 
@@ -42,8 +46,8 @@ int main(int argc, char* argv[])
 		auto start_time = std::chrono::system_clock::now();
 		std::vector<uint8_t> idata(IMAGE_WIDTH * IMAGE_HEIGHT * 3);
 		MPI_Comm_size(MPI_COMM_WORLD, &numTasks);	//Get number of tasks
-
-		splitWork(idata, IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_WIDTH * 3, (1 << maxiter_));
+		numTasks -= 1;
+		splitWork(idata, IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_WIDTH * 3, (1 << maxiter_), numTasks);
 		
 		auto end_time = std::chrono::system_clock::now();
 		Image img(IMAGE_WIDTH, IMAGE_HEIGHT, "RGB8");
@@ -53,48 +57,77 @@ int main(int argc, char* argv[])
 		std::cout << "Total time: " << msec / 1000.0 << " seconds\n";
 	}
 	else			//task
-		compute();
+		compute(rank);
 
     return 0;
 }
 
 void splitWork(std::vector<uint8_t>& data, int w, int h, int stride, int maxiter, int numTasks)
 {
+	int sizeOfData = data.size() * sizeof(data[0]);
+	globalData.clear(); globalData.resize(sizeOfData);
+	std::copy(data.begin(), data.end(), globalData.begin());
+	std::cout << "starting send numTasks: " << numTasks << std::endl;
+
 	int heightMul = h / numTasks;
-	for (int i = 0; i < numTasks; i++)
+	maxHeight = h;
+	for (int i = 1; i <= numTasks; i++)
 	{
 		workLoad* task = new workLoad();
-		task->data = data;
-		task->w = w; task->h = heightMul * (i+1);			//set width of image, and height to do work on
-		task->miter = maxiter; task->stride = stride;		//set maxiter and stride
-		task->x = 0; task->y = (h / numTasks) * i;			//set starting x and starting y
-		int size = sizeof(data.data) + (sizeof(int) * 6);	//get structs size
-		MPI_Send(&task, size, MPI_BYTE, );
+		task->w = w; task->h = heightMul * (i);									//set width of image, and height to do work on
+		task->miter = maxiter; task->stride = stride;							//set maxiter and stride
+		task->y = (h / numTasks) * i-1;											//set starting x and starting y
+		
+		MPI_Send(&task, sizeof(task), MPI_BYTE, i, 0, MPI_COMM_WORLD);			//send size to work on
 	}
+	std::vector<uint8_t> newData;
+	newData.resize(sizeOfData);
+	for (int i = 1; i <= numTasks; i++)
+	{
+		char msg[4];
+		MPI_Status status;
+		int sizeOfData;
+
+		MPI_Probe(0, 0, MPI_COMM_WORLD, &status);
+		MPI_Get_count(&status, MPI_BYTE, &sizeOfData);
+		MPI_Recv(msg, sizeOfData, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
+	}
+
+	data = newData;
+	data.clear(); data.resize(sizeOfData);
+	std::copy(globalData.begin(), globalData.end(), data.begin());
+	std::cout << "Done" << std::endl;
 }
 
 //this is where the actual Mandelbrot computation takes place.
-void compute()
+void compute(int rank)
 {
+	std::cout << rank << std::endl;
+	workLoad task;
+	int sizeOfData;
+	MPI_Status status;
+	MPI_Recv(&task, sizeof(int)*5, MPI_BYTE, rank, 0, MPI_COMM_WORLD, &status);
+	std::cout << "got data" << std::endl;
+
 	//std::vector<uint8_t>& data, int w, int h, int stride, int maxiter
-	std::vector<uint8_t> data;
-	int maxiter, stride;
-	int h, w;
-    auto deltaY = (ymax - ymin) / (double)(h);
+	int maxiter = task.miter, stride = task.stride;
+	int h = task.h, w = task.w;
+    auto deltaY = (ymax - ymin) / (double)(maxHeight);
     auto deltaX = (xmax - xmin) / (double)(w);
     int x, y;
     double px, py;
     uint8_t rgb[3];
-    for(y = 0,py = ymin; y < h; y++,py += deltaY) {
+    for(y = task.y,py = ymin; y < h; y++,py += deltaY) {
         int idx = y * stride;
         for(x = 0,px = xmin; x < w; x++,px += deltaX) {
             int iter = iterationsToInfinity(px, py, maxiter);
             mapColor(iter, maxiter, rgb);
-            data[idx++] = rgb[0];
-            data[idx++] = rgb[1];
-            data[idx++] = rgb[2];
+            globalData[idx++] = rgb[0];
+			globalData[idx++] = rgb[1];
+			globalData[idx++] = rgb[2];
         }
     }
+	MPI_Send("Done", sizeof(char)*4, MPI_CHAR, 0, 0, MPI_COMM_WORLD);		//send data back
 }
 
 //for point px,py, return the number of iterations it takes
